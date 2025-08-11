d19fc1a302c05c42f32525a039f4970916a76183
Jahaziel01 <104876481+Jahaziel01@users.noreply.github.com>
2025-08-11 11:32:36 -0400

    UPDATE: update fetch.js

--- fetch.js
+++ fetch.js
@@ -4,7 +4,7 @@ async function getData() {
         const response = await fetch('https://jsonplaceholder.typicode.com/posts');
         const data = await response.json();
         
-        const filteredData = data.filter(item => item.id <= 20);
+        const filteredData = data.filter(item => item.id <= 10);
         return filteredData;
     } catch (error) {
         console.error(error);
